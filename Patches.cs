c7c2d7ec9274c071c84e874d1f6dca0c22a11fc4
lwilson <lwilson@citystateentertainment.com>
2025-05-02 22:19:31 -0700

    changing idea, no more two-phase setup, just going to override existing phase logic and add a shoot on left-click while in raise phase later

--- Patches.cs
+++ Patches.cs
@@ -10,6 +10,96 @@ namespace MonsieurMeh.Mods.TLD.BearSpearReloaded
 {
     internal class Patches
     {
+
+        [HarmonyPatch(typeof(vp_FPSPlayer), "HandleBearSpearInput", new Type[] { typeof(PlayerManager) })]
+        internal class vp_FPSCameraPatches_HandleBearSpearInput
+        {
+            public static bool Prefix(vp_FPSPlayer __instance, PlayerManager pm)
+            {
+                if (GameManager.m_PlayerStruggle.m_Active && GameManager.m_PlayerStruggle.m_IsInSpearStruggle)
+                {
+                    return false;
+                }
+
+                //add sequence here for proximity minigame! or maybe require people to use LegendaryWolves and add the logic to wolf ai instead? that makes more sense...
+
+                if (GameManager.m_SuppressWeaponAim)
+                {
+                    return false;
+                }
+
+                BearSpearItem spear = pm.m_ItemInHandsInternal.m_BearSpearItem;
+
+                if (spear == null)
+                {
+                    return false;
+                }
+
+                if (InputManager.GetAltFirePressed(__instance))
+                {
+                    __instance.m_SpearZoomRequested = true;
+                    __instance.m_CancelSpearZoomRequested = false;
+                }
+
+
+                if (InputManager.GetAltFireReleased(__instance))
+                {
+                    __instance.m_SpearZoomRequested = false;
+                    __instance.m_CancelSpearZoomRequested = true;
+                }
+
+
+                if (spear.m_CurrentSpearState == BearSpearItem.SpearState.Raised && GameManager.m_PlayerMovement.m_SprintStamina <= 0.0001f)
+                {
+                    __instance.m_SpearZoomRequested = false;
+                    __instance.m_CancelSpearZoomRequested = true;
+                    return false;
+                }
+
+                if (GameManager.m_SuppressWeaponAim)
+                {
+                    return false;
+                }
+
+
+                if (__instance.m_SpearZoomRequested && !__instance.m_InZoom && !__instance.m_InteractReleaseRequiredBeforeSpearZoom && spear.m_CurrentSpearState == BearSpearItem.SpearState.None)
+                {
+                    //__instance.SetState("Zoom", true);
+                    //__instance.FPSCamera.ToggleZoom(true);
+                    //__instance.m_InZoom = true;
+                    if (spear.m_CurrentSpearState != BearSpearItem.SpearState.Setting)
+                    {
+                        spear.m_PendingSpearState = BearSpearItem.SpearState.Setting;
+                        spear.OnEnter_Setting();
+                        if (spear.m_PendingSpearState == BearSpearItem.SpearState.Setting)
+                        {
+                            spear.m_CurrentSpearState = BearSpearItem.SpearState.Setting;
+                        }
+                    }
+                    __instance.m_SpearZoomRequested = false;
+                }
+
+                if (__instance.m_CancelSpearZoomRequested)
+                {
+                    if (spear.m_CurrentSpearState == BearSpearItem.SpearState.None || spear.m_CurrentSpearState == BearSpearItem.SpearState.Raised)
+                    {
+                        __instance.m_InteractReleaseRequiredBeforeSpearZoom = false;
+                        __instance.m_CancelSpearZoomRequested = false;
+                        //__instance.MaybeCancelZoomInternal();
+                    }
+                }
+
+                return false;
+            }
+        }
+
+
+
+        // First pass, still allows classic "setting" with left mouse but adds a "shoot spear" trigger when right + left. 
+        // Decided I wanted to get rid of classic setting and do an action-button-sequence minigame on proximity while holding
+        // and shift existing logic to "aiming" since "raise" still makes a lot of sense in the context of spear throwing, and i dont want to add a second phase to this mess
+
+        /*
         [HarmonyPatch(typeof(vp_FPSPlayer), "HandleBearSpearInput", new Type[] { typeof(PlayerManager) })]
         internal class vp_FPSCameraPatches_HandleBearSpearInput
         {
@@ -40,7 +130,7 @@ namespace MonsieurMeh.Mods.TLD.BearSpearReloaded
                         __instance.m_InteractReleaseRequiredBeforeSpearZoom = false;
                         __instance.m_CancelSpearZoomRequested = false;
                         __instance.MaybeCancelZoomInternal();
-                        Log("pewpew!"); 
+                        ShootSpear(spear);
                         return false;
                     }
                     else if (InputManager.GetAltFireReleased(__instance))
@@ -60,6 +150,7 @@ namespace MonsieurMeh.Mods.TLD.BearSpearReloaded
                     __instance.SetState("Zoom", true);
                     __instance.FPSCamera.ToggleZoom(true);
                     __instance.m_InZoom = true;
+                    return false;
                 }
 
 
@@ -119,6 +210,13 @@ namespace MonsieurMeh.Mods.TLD.BearSpearReloaded
 
                 return false;
             }
+
+
+            private static void ShootSpear(BearSpearItem spear)
+            {
+
+            }
         }
+        */
     }
 }
